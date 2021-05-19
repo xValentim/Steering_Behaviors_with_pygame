9a702255f5d8371ecdcb7594948a9397cd03ac3a
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-05-18 21:50:08 -0300

    Atualização - Gerador de graficos ok!

--- graficos.py
+++ graficos.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tabela = pd.read_csv('data10.csv')
+tabela = pd.read_csv('data/data10.csv')
 lista_generation = tabela['generation']
 lista_life_time = tabela['average life time']
 
-tabela2 = pd.read_csv('data11.csv')
+tabela2 = pd.read_csv('data/data11.csv')
 lista_generation2 = tabela2['generation']
 lista_life_time2 = tabela2['average life time']
 
