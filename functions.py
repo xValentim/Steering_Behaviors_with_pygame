7ec03b63f892e5d6b762f954ee72461eb1941a17
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-05-23 16:25:29 -0300

    Atualização - to_draw_vehicles

--- functions.py
+++ functions.py
@@ -63,7 +63,23 @@ def new_generation(start_vehicles=50, start_food=50, start_poison=50):
     
 # TODO
 def to_draw_vehicle_picture(v1, vehicles, window):
-    pass
+    health_i = round(v1.health) + 1
+    if health_i < 1:
+        health_i = 1
+    elif health_i > 7:
+        health_i = 7
+    k = float(v1.r)
+    vehicles[health_i] = pygame.transform.scale(vehicles_base[health_i], (int(k * 30), int(k * 16)))
+
+    teta = v1.velocity.as_polar()
+    teta = teta[1]
+    
+    vehicles[health_i] = pygame.transform.rotate(vehicles[health_i], -teta)
+    
+    rect = vehicles[health_i].get_rect()
+    rect.center = v1.position
+    
+    window.blit(vehicles[health_i], rect)
 
 # TODO
 def to_draw_vehicle_polygon():
