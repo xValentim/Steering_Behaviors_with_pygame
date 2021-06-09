b77f364bc3134d381664ab0b20dd5bbe18d36209
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-06-09 14:27:08 -0300

    Atualização do codigo com pandas

--- graficos.py
+++ graficos.py
@@ -9,9 +9,10 @@ tabela2 = pd.read_csv('data/data11.csv')
 lista_generation2 = tabela2['generation']
 lista_life_time2 = tabela2['average life time']
 
-plt.plot(lista_generation, lista_life_time)
-plt.plot(lista_generation2, lista_life_time2)
+plt.plot(lista_generation, lista_life_time, label='Algoritmo genético')
+plt.plot(lista_generation2, lista_life_time2, label='Algoritmo aleatório')
 plt.grid(True)
+plt.legend()
 plt.xlabel('Generation')
 plt.ylabel('Average life time')
 plt.show()
